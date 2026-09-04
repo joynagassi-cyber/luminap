fcdc65c78b534176a227d3dc23ff85081f098c77
Dyad <joynagassi@gmail.com>
2026-09-04 20:37:38 +0100

    (1 files changed)

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
         <AppProvider>
           <AppRoutes />
         </AppProvider>
